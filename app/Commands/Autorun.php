bf21b3fbdc6ddd87481dcf6e0ce45416cfff2679
Eugene Mah <eugenemah@gmail.com>
2022-09-07 08:07:25 -0400

    Remove $xRef and $yRef loops

--- app/Commands/Autorun.php
+++ app/Commands/Autorun.php
@@ -39,12 +39,6 @@ class Autorun extends Command
 #            [30, 178.6, 73.2, 83],
         ];
 
-        $xRange[10] = [-4, 4];
-        $xRange[15] = [-4, 4];
-        $xRange[30] = [-4, 4];
-        $yRange[10] = [-4, 4];
-        $yRange[15] = [-4, 4];
-        $yRange[30] = [-4, 4];
         $zRange[10] = [57, 64];
         $zRange[15] = [72, 80];
         $zRange[30] = [80, 88];
@@ -61,69 +55,65 @@ class Autorun extends Command
                 for ($height = 1; $height <= 16; $height += 2) {
                     for ($width = 1; $width <= 16; $width += 2) {
                         for ($filtA = 2; $filtA <= 6; $filtA++) {
-                            for ($xRef = $xRange[$age][0]; $xRef <= $xRange[$age][1]; $xRef++) {
-                                for ($yRef = $yRange[$age][0]; $yRef <= $yRange[$age][1]; $yRef++) {
-                                    for ($zRef = $zRange[$age][0]; $zRef <= $zRange[$age][1]; $zRef++) {
-                                        for ($angle = 0; $angle < 360; $angle++) {
-                                            $header = sprintf($hdrFormat, $age, $kv, $xRef, $yRef, $zRef, $filtA, $width, $height, $angle);
-                                            $filename = sprintf($fnFormat, $age, $kv, $xRef, $yRef, $zRef, $filtA, $width, $height, $angle);
-                                            /*
-                                             * When the simulations get interrupted for whatever reason,
-                                             * (invalid parameters, crash, etc), it would be nice to
-                                             * be able to restart where we left off.
-                                             *
-                                             * Only way I can think of to do that right now is to
-                                             * check if $filename.dfR file exists.  If it does we can skip
-                                             * the current iteration of the loop.
-                                             * If it doesn't exist, do the simulation.
-                                             */
-                                            $startTime = microtime(true);
-                                            if (file_exists('/home/eugenem/.wine/drive_c/Program Files (x86)/PCXMC/MCRUNS/'.$filename.'.dfR')) {
-                                                $this->info('Skipping '.$header);
-                                                continue;
-                                            } else {
-                                                $content = <<<EOD
-                                                                    Header text: {$header}
-                                                                     Projection: {$angle}
-                                                                     Obl. Angle:                        0.0000
-                                                                            Age: {$age}
-                                                                         Length: {$length}
-                                                                           Mass: {$mass}
-                                                                Arms in phantom:                             1
-                                                                            FRD:                       40.0000
-                                                               X-ray beam width: {$width}
-                                                              X-ray beam height: {$height}
-                                                                            FSD:                       37.2000
-                                                             Beam width at skin:                       14.8800
-                                                            Beam height at skin:                        3.0969
-                                                                           Xref: {$xRef}
-                                                                           Yref: {$yRef}
-                                                                           Zref: {$zRef}
-                                                          E-levels (Max.en./10):                            15
-                                                                         NPhots:                         20000
-                                                                             kV: {$kv}
-                                                                     AnodeAngle:                             5
-                                                                   Filter A (Z):                            13
-                                                                  Filter A (mm): {$filtA}
-                                                                   Filter B (Z):                            29
-                                                                  Filter B (mm):                             0
-                                                            Input dose quantity:                           DAP
-                                                               Input dose value:                           1.0
-                                                               Output file name: {$filename}
-                                                EOD;
+                            for ($zRef = $zRange[$age][0]; $zRef <= $zRange[$age][1]; $zRef++) {
+                                for ($angle = 0; $angle < 360; $angle++) {
+                                    $header = sprintf($hdrFormat, $age, $kv, $xRef, $yRef, $zRef, $filtA, $width, $height, $angle);
+                                    $filename = sprintf($fnFormat, $age, $kv, $xRef, $yRef, $zRef, $filtA, $width, $height, $angle);
+                                    /*
+                                     * When the simulations get interrupted for whatever reason,
+                                     * (invalid parameters, crash, etc), it would be nice to
+                                     * be able to restart where we left off.
+                                     *
+                                     * Only way I can think of to do that right now is to
+                                     * check if $filename.dfR file exists.  If it does we can skip
+                                     * the current iteration of the loop.
+                                     * If it doesn't exist, do the simulation.
+                                     */
+                                    $startTime = microtime(true);
+                                    if (file_exists('/home/eugenem/.wine/drive_c/Program Files (x86)/PCXMC/MCRUNS/'.$filename.'.dfR')) {
+                                        $this->info('Skipping '.$header);
+                                        continue;
+                                    } else {
+                                        $content = <<<EOD
+                                                            Header text: {$header}
+                                                             Projection: {$angle}
+                                                             Obl. Angle:                        0.0000
+                                                                    Age: {$age}
+                                                                 Length: {$length}
+                                                                   Mass: {$mass}
+                                                        Arms in phantom:                             1
+                                                                    FRD:                       40.0000
+                                                       X-ray beam width: {$width}
+                                                      X-ray beam height: {$height}
+                                                                    FSD:                       37.2000
+                                                     Beam width at skin:                       14.8800
+                                                    Beam height at skin:                        3.0969
+                                                                   Xref:                             0
+                                                                   Yref:                             0
+                                                                   Zref: {$zRef}
+                                                  E-levels (Max.en./10):                            15
+                                                                 NPhots:                         20000
+                                                                     kV: {$kv}
+                                                             AnodeAngle:                             5
+                                                           Filter A (Z):                            13
+                                                          Filter A (mm): {$filtA}
+                                                           Filter B (Z):                            29
+                                                          Filter B (mm):                             0
+                                                    Input dose quantity:                           DAP
+                                                       Input dose value:                           1.0
+                                                       Output file name: {$filename}
+                                        EOD;
 
-                                                $fh = fopen("/home/eugenem/.wine/drive_c/Program Files (x86)/PCXMC/MCRUNS/Autocalc.dfR", 'w+');
-                                                fwrite($fh, $content);
-                                                fclose($fh);
-                                                exec("/usr/bin/wine /home/eugenem/.wine/drive_c/Program\ Files\ \(x86\)/PCXMC/PCXMC20Rotation.exe");
-                                            }
+                                        $fh = fopen("/home/eugenem/.wine/drive_c/Program Files (x86)/PCXMC/MCRUNS/Autocalc.dfR", 'w+');
+                                        fwrite($fh, $content);
+                                        fclose($fh);
+                                        exec("/usr/bin/wine /home/eugenem/.wine/drive_c/Program\ Files\ \(x86\)/PCXMC/PCXMC20Rotation.exe");
+                                    }
 
-                                            $endTime = microtime(true);
-                                            $this->info(round(($endTime - $startTime)/60, 4).' - '.$filename);
-                                        } // $angle
-                                    } // $zRef
-                                } // $yRef
-                            } // $xRef
+                                    $endTime = microtime(true);
+                                    $this->info(round(($endTime - $startTime)/60, 4).' - '.$filename);
+                                } // $angle
+                            } // $zRef
                         } // $filtA
                     } // $width
                 } // $height
